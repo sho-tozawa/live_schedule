2e49652e23cb518f7605a243e336432115552149
tozawashou <afgab>
2019-06-13 09:12:32 +0900

    ログイン用のアクション、ルーティング、ビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :live_houses
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  collection do
+    login :login_form
+  end
 end
