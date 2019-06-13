f671edb9429c5c2a814ce5bc3e856501d64c3c5d
tozawashou <afgab>
2019-06-13 16:31:05 +0900

    loginアクションの追加とログイン中のユーザーIDの表示設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email],password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to ("users/index")
     else
