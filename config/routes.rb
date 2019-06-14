4d54830307ad1a4480e3ec115ec7446a8f1101e5
tozawashou <afgab>
2019-06-14 23:20:30 +0900

    before_actionの設定とloginルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "users#login_form"
   get "login" => "users#login_form"
+  post "login" => "users#login"
   post "logout" => "users#logout"
 end
