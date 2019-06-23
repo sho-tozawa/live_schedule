054243a5adcbf6cf00d1f2013fd682588457380e
tozawashou <afgab>
2019-06-23 15:23:26 +0900

    forbid_login_userのリダイレクト先を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def forbid_login_user
     if @current_user
       flash[:notice] = "すでにログインしています"
-      redirect_to("/users/show")
+      redirect_to("/users/#{@user.id}")
     end
   end
 end
