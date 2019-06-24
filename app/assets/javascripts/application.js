984e66d63f7017ace6ff8e62e050c52c4ecd535c
tozawashou <afgab>
2019-06-24 21:43:01 +0900

    application.jsにイベントを表示する関数を定義

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,20 @@
 //= require moment
 //= require fullcalendar
 //= require_tree .
+
+$(function () {
+    function eventCalendar() {
+        return $('#calendar').fullCalendar({});
+    };
+    function clearCalendar() {
+        $('#calendar').html('');
+    };
+    $(document).on('turbolinks:load', function () {
+        eventCalendar();
+    });
+    $(document).on('turbolinks:before-cache', clearCalendar);
+
+    $('#calendar').fullCalendar({
+        events: '/events.json'
+    });
+});
