f5bce43bcaded5cba11acadc9c4d7170f38900eb
tozawashou <afgab>
2019-06-25 22:38:21 +0900

    indexアクションを記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,12 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events }
+      format.json { render :json => @events }
+    end
   end
 
   def show
