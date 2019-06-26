8e42d0f665bb19972f1ac0e7552041a2fcd2de2c
tozawashou <afgab>
2019-06-26 21:34:23 +0900

    updateアクション、destroyアクションの作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,4 +32,24 @@ class EventsController < ApplicationController
       end
     end
   end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html {redirect_to @event, notice: 'Event was successfully updated.'}
+        format.json {render :show, status: :ok, location: @event}
+      else
+        format.html {render :edit}
+        format.json {render json: @event.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 end
